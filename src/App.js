5f10d58d3e97bfcb6358c1d5a7c0ea8a3b617086
Aaron Fraint <aaronfraint@icloud.com>
2023-05-12 16:16:59 -0400

    update on new station name

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { useEffect, useState } from "react";
-
 import { Container, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
+
+import "./App.css";
 import { ListOfTimes } from "./components/single_station_times/listOfTimes";
 import { SelectStation } from "./components/shared/selectStation";
+
 function App() {
+  const selectedStation = useSelector((state) => state.app.selectedStation);
+  // const dispatch = useDispatch();
+
   const [apiData, setApiData] = useState("");
 
   async function getDataFromApi() {
     const response = await fetch(
-      "https://patco-api.planninglab.org/api/v0/timetable/?station_name=haddonfield&direction=wb"
+      `https://patco-api.planninglab.org/api/v0/timetable/?station_name=${selectedStation}&direction=wb`
     );
     const jsonData = await response.json();
     return jsonData;
@@ -20,7 +24,7 @@ function App() {
 
   useEffect(() => {
     getDataFromApi().then((data) => setApiData(data.upcoming_times));
-  }, []);
+  }, [selectedStation]);
 
   return (
     <Container maxWidth="sm">
