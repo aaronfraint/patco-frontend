e19e8c2e89836b65ba0234b3512e1de6bd378312
Aaron Fraint <aaronfraint@icloud.com>
2023-05-12 16:37:42 -0400

    improve labels

--- src/components/shared/selectStation.js
+++ src/components/shared/selectStation.js
@@ -26,19 +26,19 @@ function SelectStation() {
         label="Select a station"
         onChange={handleChange}
       >
-        <MenuItem value={"15_16th_and_locust"}>15_16th_and_locust</MenuItem>
-        <MenuItem value={"12_13th_and_locust"}>12_13th_and_locust</MenuItem>
-        <MenuItem value={"9_10th_and_locust"}>9_10th_and_locust</MenuItem>
-        <MenuItem value={"8th_and_market"}>8th_and_market</MenuItem>
-        <MenuItem value={"city_hall"}>city_hall</MenuItem>
-        <MenuItem value={"broadway"}>broadway</MenuItem>
-        <MenuItem value={"ferry_ave"}>ferry_ave</MenuItem>
-        <MenuItem value={"collingswood"}>collingswood</MenuItem>
-        <MenuItem value={"westmont"}>westmont</MenuItem>
-        <MenuItem value={"haddonfield"}>haddonfield</MenuItem>
-        <MenuItem value={"woodcrest"}>woodcrest</MenuItem>
-        <MenuItem value={"ashland"}>ashland</MenuItem>
-        <MenuItem value={"lindenwold"}>lindenwold</MenuItem>
+        <MenuItem value={"15_16th_and_locust"}>15/16th & Locust</MenuItem>
+        <MenuItem value={"12_13th_and_locust"}>12/13th & Locust</MenuItem>
+        <MenuItem value={"9_10th_and_locust"}>9/10th & Locust</MenuItem>
+        <MenuItem value={"8th_and_market"}>8th & Market</MenuItem>
+        <MenuItem value={"city_hall"}>City Hall</MenuItem>
+        <MenuItem value={"broadway"}>Broadway</MenuItem>
+        <MenuItem value={"ferry_ave"}>Ferry Ave</MenuItem>
+        <MenuItem value={"collingswood"}>Collingswood</MenuItem>
+        <MenuItem value={"westmont"}>Westmont</MenuItem>
+        <MenuItem value={"haddonfield"}>Haddonfield</MenuItem>
+        <MenuItem value={"woodcrest"}>Woodcrest</MenuItem>
+        <MenuItem value={"ashland"}>Ashland</MenuItem>
+        <MenuItem value={"lindenwold"}>Lindenwold</MenuItem>
       </Select>
     </FormControl>
   );
