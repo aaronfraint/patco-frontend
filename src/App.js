65cd5a35eff3ce6757db5b854b029504babdff0d
Aaron Fraint <aaronfraint@icloud.com>
2023-05-12 16:28:28 -0400

    add api data to state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
 import { Container, Typography } from "@mui/material";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import "./App.css";
 import { ListOfTimes } from "./components/single_station_times/listOfTimes";
 import { SelectStation } from "./components/shared/selectStation";
 
+import { setApiData } from "./store/appSlice";
+
 function App() {
   const selectedStation = useSelector((state) => state.app.selectedStation);
-  // const dispatch = useDispatch();
+  const apiData = useSelector((state) => state.app.apiData);
+  const dispatch = useDispatch();
 
-  const [apiData, setApiData] = useState("");
+  // const [apiData, setApiData] = useState("");
 
   async function getDataFromApi() {
     const response = await fetch(
@@ -20,10 +23,10 @@ function App() {
     return jsonData;
   }
 
-  // getDataFromApi();
+  getDataFromApi();
 
   useEffect(() => {
-    getDataFromApi().then((data) => setApiData(data.upcoming_times));
+    getDataFromApi().then((data) => dispatch(setApiData(data.upcoming_times)));
   }, [selectedStation]);
 
   return (
--- src/store/appSlice.js
+++ src/store/appSlice.js
@@ -6,25 +6,19 @@ export const appSlice = createSlice({
     value: 0,
     foo: "bar",
     selectedStation: "haddonfield",
+    apiData: null,
   },
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value -= 1;
-    },
     setSelectedStation: (state, action) => {
       state.selectedStation = action.payload;
+    },
+    setApiData: (state, action) => {
+      state.apiData = action.payload;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, setSelectedStation } = appSlice.actions;
+export const { setSelectedStation, setApiData } = appSlice.actions;
 
 export default appSlice.reducer;
