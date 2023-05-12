778a0a43c90bee8e0723a1cde736ebbda2230607
Aaron Fraint <aaronfraint@icloud.com>
2023-05-12 15:59:59 -0400

    add mui

--- src/App.js
+++ src/App.js
@@ -1,47 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
+import { Container, Typography } from "@mui/material";
+import { ListOfTimes } from "./components/single_station_times/listOfTimes";
+import { SelectStation } from "./components/shared/selectStation";
 function App() {
-
-  const [apiData, setApiData] = useState('');
+  const [apiData, setApiData] = useState("");
 
   async function getDataFromApi() {
-    const response = await fetch("https://patco-api.planninglab.org/api/v0/timetable/?station_name=haddonfield&direction=wb");
+    const response = await fetch(
+      "https://patco-api.planninglab.org/api/v0/timetable/?station_name=haddonfield&direction=wb"
+    );
     const jsonData = await response.json();
-    return jsonData
+    return jsonData;
   }
 
   // getDataFromApi();
 
   useEffect(() => {
-    getDataFromApi()
-    .then(data =>
-
-      setApiData(data.upcoming_times)
-      );
-   }, [])
- 
-  
+    getDataFromApi().then((data) => setApiData(data.upcoming_times));
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello world
-          {apiData}
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Typography variant="h1">PATCO Schedule</Typography>
+      <SelectStation />
+      {apiData && <ListOfTimes dataList={apiData} />}
+    </Container>
   );
 }
 
